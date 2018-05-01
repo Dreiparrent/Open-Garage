31ab5cb2704557b3ac1f1b33d5314a94affccaa4
DreiParrent <dreiparrent@gmail.com>
2018-05-01 02:17:40 -0600

    Day 16, Maps and optimization updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SwUpdateService } from './sw-update.service';
 import { environment } from '../environments/environment.prod';
+// import '../assets/css/bootstrap.min.css';
 
 @Component({
     selector: 'app-root',
@@ -13,26 +14,20 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
-        /*
         const head = document.getElementsByTagName('head')[0];
         const style = head.getElementsByTagName('style')[0];
-        if (environment.production) {
-            const link2 = document.createElement('link');
-            // link2.href =
-            link2.href = 'styles.*.css';
-            link2.rel = 'stylesheet';
-            link2.type = 'text/css';
-            head.insertBefore(link2, style);
-        }
-        */
+
+        const link1 = document.createElement('link');
+        link1.href = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css';
+        link1.rel = 'stylesheet';
+        head.insertBefore(link1, style);
+
+        const link2 = document.createElement('link');
+        link2.href = 'https://fonts.googleapis.com/icon?family=Material+Icons';
+        link2.rel = 'stylesheet';
+        head.insertBefore(link2, style);
+        // head.innerHTML += '<link rel="preload" href="https://fonts.googleapis.com/icon?family=Material+Icons" as="style">';
         /*
-        const link = document.createElement('link');
-        link.href = 'https://fonts.googleapis.com/icon?family=Material+Icons';
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-
-        head.insertBefore(link, style);
-
         if (environment.production) {
             const link2 = document.createElement('link');
             // link2.href =
